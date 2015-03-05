f4a577111b5c0a5ea0ae4f8a62d81a6685dbccbd
deadtomgc <deadtomgc@gmail.com>
2015-03-05 09:58:37 -0500

    initial commit

---
+++ FTCHCOM/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FCTH_Descriptor;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace FTCHpy
+{
+    public class FTCHcalc
+    {
+        double[] FCTHTable = new double[192];
+        int[,] img;
+        public void setImageSize(int i, int j)
+        {
+            img = new int[i,j];
+
+        }
+        public void setVal(int i, int j, int color)
+        {
+            img[i,j] = color;
+
+        }
+        public void calc(){
+            FCTH_Descriptor.FCTH get = new FCTH_Descriptor.FCTH();
+            Bitmap bmp = new Bitmap(img.GetLength(0),img.GetLength(1));
+
+            for (int i = 0; i < img.GetLength(0); i++)
+            {
+                for (int j = 0; j < img.GetLength(1); j++)
+                {
+                    bmp.SetPixel(i, j, Color.FromArgb(img[i,j]));
+                }
+            }
+            FCTHTable = get.Apply(bmp,2);
+            
+        }
+        public double result(int i)
+        {
+            return FCTHTable[i];
+        }
+    }
+}
